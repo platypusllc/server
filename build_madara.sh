2de08f65fad632ec218d1d303c9a196dc9c56cbb
Pras Velagapudi <psigen@gmail.com>
2015-03-10 02:17:25 -0400

    Fixed build script to actually work with modern NDK.

--- build_madara.sh
+++ build_madara.sh
@@ -23,6 +23,9 @@
 # $ ./build_madara.sh
 #
 
+# Store the script directory.
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 # Return an error if the NDK path was not set.
 if [ -z "$NDK" ]
 then
@@ -32,16 +35,19 @@ then
 fi
 
 # Set up temporary install location.
-export BUILD_ROOT="/tmp/madara"
+BUILD_ROOT="/tmp/madara"
 rm -rf "${BUILD_ROOT}"
 mkdir -p "${BUILD_ROOT}"
 
-# Build the standalone toolchain right now.
-echo "Building standalone NDK toolchain."
+# Build the standalone NDK toolchain.
+echo "Building standalone NDK toolchain..."
 export NDK_TOOLCHAIN="${BUILD_ROOT}/ndk_toolchain"
 export NDK_BIN="${NDK_TOOLCHAIN}/bin"
 pushd "${NDK}"
-./build/tools/make-standalone-toolchain.sh --platform=android-21 --arch=arm --install-dir="${NDK_TOOLCHAIN}"
+./build/tools/make-standalone-toolchain.sh \
+	--toolchain=arm-linux-androideabi-4.8 \
+	--arch=arm --platform=android-14 \
+	--install-dir="${NDK_TOOLCHAIN}"
 if [ $? -ne 0 ]
 then
     echo "ERROR: Unable to build standalone NDK toolchain."
@@ -57,7 +63,7 @@ export PATH=$ACE_ROOT/bin:$MADARA_ROOT/bin:$PATH
 export PATH=$NDK_BIN:$PATH
 
 # Use all available cores.
-export CORES=$(nproc)
+CORES=$(nproc)
 
 # Make the ACE and MADARA directories in $BUILD_ROOT
 rm -rf $BUILD_ROOT/ace $BUILD_ROOT/madara
@@ -68,6 +74,7 @@ echo "Downloading ACE..."
 svn co -q svn://svn.dre.vanderbilt.edu/DOC/Middleware/sets-anon/ACE $BUILD_ROOT/ace
 cd $ACE_ROOT/ace
 echo "#include \"ace/config-android.h\"" > config.h
+echo "#define ACE_HAS_UCONTEXT_T" >> config.h
 cd $ACE_ROOT/include/makeinclude
 echo -e "versioned_so=0\nstatic_libs_only=0\ninclude \$(ACE_ROOT)/include/makeinclude/platform_android.GNU" > platform_macros.GNU
 cd $ACE_ROOT/ace
@@ -84,11 +91,17 @@ fi
 echo "Downloading MADARA..."
 git clone git://git.code.sf.net/p/madara/code $BUILD_ROOT/madara
 cd $MADARA_ROOT
+git apply "${SCRIPT_DIR}/madara_ndk.patch"
+if [ $? -ne 0 ]
+then
+    echo "ERROR: Unable to patch MADARA against latest NDK."
+    exit 4
+fi
 echo "Building MADARA..."
 perl $ACE_ROOT/bin/mwc.pl -type gnuace -features java=1,android=1,tests=0 MADARA.mwc
 make tests=0 java=1 android=1 -j $CORES
 if [ $? -ne 0 ]
 then
     echo "ERROR: Unable to build MADARA."
-    exit 4
+    exit 5
 fi
