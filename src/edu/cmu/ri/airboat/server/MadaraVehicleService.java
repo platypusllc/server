a313b00383bead52c2fdb079fffd08b1200a4ddd
Pras Velagapudi <psigen@gmail.com>
2015-03-17 00:01:42 -0400

    Added rough sketch of MADARA->VehicleServer implementation.

--- src/edu/cmu/ri/airboat/server/MadaraVehicleService.java
+++ src/edu/cmu/ri/airboat/server/MadaraVehicleService.java
@@ -1,7 +1,19 @@
 package edu.cmu.ri.airboat.server;
 
+import com.google.code.microlog4android.LoggerFactory;
+import com.madara.KnowledgeBase;
+import com.madara.KnowledgeList;
+import com.madara.KnowledgeRecord;
+import com.madara.Variables;
+import com.madara.transport.QoSTransportSettings;
+import com.madara.transport.TransportType;
+import com.madara.transport.filters.RecordFilter;
+
+import java.nio.charset.Charset;
+
 import edu.cmu.ri.crw.AsyncVehicleServer;
 import edu.cmu.ri.crw.FunctionObserver;
+import edu.cmu.ri.crw.ImageListener;
 import edu.cmu.ri.crw.PoseListener;
 import edu.cmu.ri.crw.SensorListener;
 import edu.cmu.ri.crw.VehicleServer;
@@ -9,28 +21,65 @@ import edu.cmu.ri.crw.WaypointListener;
 import edu.cmu.ri.crw.data.SensorData;
 import edu.cmu.ri.crw.data.UtmPose;
 
-import com.google.code.microlog4android.LoggerFactory;
-import com.madara.KnowledgeBase;
-import com.madara.transport.TransportSettings;
-import com.madara.transport.TransportType;
-
 /**
  * Vehicle service that exposes a vehicle via MADARA fields.
+ *
+ * This is a lightweight wrapper that monitors the state variables in a MADARA knowledge base and
+ * applies those values directly to a VehicleServer when they are changed.  Similarly, vehicle
+ * server updates are immediately set as values in this knowledge base.
  */
-public class MadaraVehicleService implements WaypointListener, PoseListener, SensorListener {
+public class MadaraVehicleService implements WaypointListener, PoseListener, SensorListener, ImageListener {
     private static final com.google.code.microlog4android.Logger logger = LoggerFactory
             .getLogger();
+    /**
+     * Handle incoming MADARA data by forwarding it to the vehicle server.
+     */
+    final RecordFilter _receivedMessageFilter = new RecordFilter() {
+        // Add an id if it doesn't exist. Strip id if it does.
+        public KnowledgeRecord filter(KnowledgeList args, Variables variables) {
+            KnowledgeRecord record = args.get(0);
+            if (args.get(1).toString().contains(".pose")) {
 
+                // Set server pose if we receive a change to the pose variable.
+                _server.setPose(getUtmPose(record, ".pose"), null);
+
+            } else if (args.get(1).toString().contains(".waypoint")) {
+
+                // Deserialize a list of waypoints and begin execution.
+                int numWaypoints = record.get(".waypoint.length");
+                UtmPose waypoints[] = new UtmPose[numWaypoints];
+                for (int i = 0; i < waypoints.length; ++i) {
+                    waypoints[i] = getUtmPose(record, ".waypoint." + i);
+                }
+                String controller = record.get("controller");
+                _server.startWaypoints(waypoints, null, null);
+            }
+
+            return result;
+        }
+    };
+
+    /** Local reference to vehicle server. */
     protected AsyncVehicleServer _server;
+
+    /** Local reference to MADARA knowledge base. */
     protected KnowledgeBase _knowledge;
 
+    /**
+     * Initialize a MADARA VehicleService which interfaces the provided VehicleServer to a MADARA
+     * knowledge base with default multicast settings.
+     *
+     * @param server the VehicleServer that should be interfaced to MADARA
+     */
     public MadaraVehicleService(VehicleServer server) {
-        TransportSettings settings = configureTransport();
+        QoSTransportSettings settings = configureTransport();
+        // TODO: where should we get the name of the vehicle?
         _knowledge = new KnowledgeBase("agent1", settings);
 
         // Register for server events.
         _server.addPoseListener(this, null);
         _server.addWaypointListener(this, null);
+        _server.addImageListener(this, null);
         _server.getNumSensors(new FunctionObserver<Integer>() {
             @Override
             public void completed(Integer numSensors) {
@@ -44,16 +93,31 @@ public class MadaraVehicleService implements WaypointListener, PoseListener, Sen
                 logger.warn("Failed to get number of vehicle sensors.");
             }
         });
+
+        // Register for MADARA data update events.
+        settings.addReceiveFilter(KnowledgeType.ALL_TYPES, _receivedMessageFilter);
     }
 
-    protected static TransportSettings configureTransport() {
+    /**
+     * Set up a standard QOS multicast transport for MADARA to use.
+     * @return a simple multicast transport settings object for a knowledge base
+     */
+    protected static QoSTransportSettings configureTransport() {
         //Create transport settings for a multicast transport
-        TransportSettings settings = new TransportSettings();
+        QoSTransportSettings settings = new QoSTransportSettings();
         settings.setHosts(new String[]{"239.255.0.1:4150"});
         settings.setType(TransportType.MULTICAST_TRANSPORT);
         return settings;
     }
 
+    /**
+     * Conversion method that takes a UTMPose and a variable name in MADARA and set the variable in
+     * the provided knowledge base to the given UtmPose.
+     *
+     * @param knowledge a knowledge base that will be updated
+     * @param knowledgePath the name of the variable in the knowledge base that should be updated
+     * @param utmPose the UTM pose that the knowledge base will be updated with
+     */
     public static void setUtmPose(KnowledgeBase knowledge, String knowledgePath, UtmPose utmPose) {
         knowledge.set(knowledgePath + ".x", utmPose.pose.getX());
         knowledge.set(knowledgePath + ".y", utmPose.pose.getY());
@@ -62,9 +126,29 @@ public class MadaraVehicleService implements WaypointListener, PoseListener, Sen
         knowledge.set(knowledgePath + ".roll", utmPose.pose.getRotation().toPitch());
         knowledge.set(knowledgePath + ".yaw", utmPose.pose.getRotation().toYaw());
         knowledge.set(knowledgePath + ".zone", utmPose.origin.zone);
-        knowledge.set(knowledgePath + ".hemisphere", utmPose.origin.isNorth ? "North": "South");
+        knowledge.set(knowledgePath + ".hemisphere", utmPose.origin.isNorth ? "North" : "South");
     }
 
+    /**
+     * Conversion method that takes a variable name in a MADARA Knowledge Record and extracts a
+     * UtmPose from that variable.
+     *
+     * @param record the knowledge record containing the UTM pose
+     * @param knowledgePath the name of the variable in the knowledge base that contains the pose
+     * @return the UTM pose that was stored in the specified variable
+     */
+    public static UtmPose getUtmPose(KnowledgeRecord record, String knowledgePath) {
+        // Convert a knowledge record into a UTM pose.
+        // TODO: how does this conversion work?
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    /**
+     * Receive changes in waypoint state from the vehicle server and update the MADARA waypoint
+     * variables to match.
+     *
+     * @param waypointState the latest state of vehicle waypoint execution
+     */
     @Override
     public void waypointUpdate(final VehicleServer.WaypointState waypointState) {
         _server.getWaypoints(new FunctionObserver<UtmPose[]>() {
@@ -92,4 +176,11 @@ public class MadaraVehicleService implements WaypointListener, PoseListener, Sen
     public void receivedSensor(SensorData sensorData) {
         _knowledge.set(".sensor." + sensorData.channel, sensorData.data);
     }
+
+    @Override
+    public void receivedImage(byte[] bytes) {
+        // TODO: how do you set an IMAGE_JPEG to a variable in the knowledge base?
+        _knowledge.set(".image", new String(bytes));
+    }
 }
+
