9b1103f559915a53ee869719e7c1170088dbb3e5
Pras Velagapudi <psigen@gmail.com>
2015-11-12 20:46:36 -0500

    Added unit tests for merge function.

---
+++ tests/platypus/server/test_util.py
@@ -0,0 +1,43 @@
+import platypus.server.util
+from unittest import TestCase
+
+
+class UtilTest(TestCase):
+    def test_merge(self):
+        A = {
+            "m0": {"type": "HobbyKing"},
+            "m1": {
+                "v": 1.0,
+                "type": "HobbyKing",
+                "enabled": False
+            },
+            "m2": "test_motor",
+            "s0": "test_sensor"
+        }
+
+        B = {
+            "m0": {"v": 2.0},
+            "m1": {
+                "v": 3.0,
+                "enabled": None,
+                "comment": "test_attr"
+            },
+            "s0": None,
+            "s1": {"type": "sensor"}
+        }
+
+        platypus.server.util.merge(A, B)
+
+        # Make sure the correct keys are in the result.
+        self.assertEqual(A["m0"]["type"], "HobbyKing")
+        self.assertEqual(A["m0"]["v"], 2.0)
+
+        self.assertEqual(A["m1"]["v"], 3.0)
+        self.assertEqual(A["m1"]["type"], "HobbyKing")
+        self.assertNotIn("enabled", A["m1"])
+        self.assertEqual(A["m1"]["comment"], "test_attr")
+
+        self.assertEqual(A["m2"], "test_motor")
+        self.assertNotIn("s0", A)
+
+        self.assertEqual(A["s1"]["type"], "sensor")
