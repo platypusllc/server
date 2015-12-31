bc38efa8b573bc346a6690dd41fb7f08b54f0a36
Pras Velagapudi <psigen@gmail.com>
2015-12-31 02:39:25 -0500

    Added initial implementation of one-touch launcher.

---
+++ src/com/platypus/android/server/LauncherFragment.java
@@ -0,0 +1,23 @@
+package com.platypus.android.server;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment that controls the starting and stopping of the vehicle server.
+ *
+ * This fragment provides a simple full-screen one-touch interface for starting and stopping the
+ * vehicle server.  While this screen is up, it can also lock the display on and disables button
+ * functionality, making it easier to ensure that the server stays up and running.
+ */
+public class LauncherFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.launcher_view, container, false);
+    }
+}
