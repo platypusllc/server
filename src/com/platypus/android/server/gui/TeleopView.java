f572e6a6472c4e000369d064159245bafcb8fa99
Pras Velagapudi <psigen@gmail.com>
2016-01-19 23:10:47 -0500

    Added skeleton to actually debug panel to server.

--- src/com/platypus/android/server/gui/TeleopView.java
+++ src/com/platypus/android/server/gui/TeleopView.java
@@ -5,10 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
-import android.graphics.Rect;
 import android.graphics.RectF;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -21,6 +20,8 @@ import com.platypus.crw.data.Twist;
  * @author pkv
  */
 public class TeleopView extends View {
+    private static final int NUM_RINGS = 3;
+
     private Paint mPointerPaint = new Paint();
     private Paint mRingEnabledPaint = new Paint();
     private Paint mRingDisabledPaint = new Paint();
@@ -28,6 +29,10 @@ public class TeleopView extends View {
     protected AsyncVehicleServer mServer = null;
     protected PointF mFinger = null;
     protected final Object mFingerLock = new Object();
+
+    protected float mWidth;
+    protected float mHeight;
+    protected RectF[] mRings;
 
     public TeleopView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -42,6 +47,10 @@ public class TeleopView extends View {
         mRingEnabledPaint.setColor(Color.YELLOW);
         mRingEnabledPaint.setStyle(Paint.Style.STROKE);
         mRingEnabledPaint.setStrokeWidth(10.0f);
+
+        mRings = new RectF[NUM_RINGS];
+        for (int i = 0; i < NUM_RINGS; ++i)
+            mRings[i] = new RectF();
     }
 
     public synchronized void getVehicleServer(AsyncVehicleServer server) {
@@ -56,11 +65,23 @@ public class TeleopView extends View {
         }
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mWidth = w;
+        mHeight = h;
+
+        for (int i = 0; i < NUM_RINGS; ++i) {
+            mRings[i].set(0, 0, mWidth, mHeight);
+            mRings[i].inset(
+                    i * mWidth / (2.0f * NUM_RINGS),
+                    i * mHeight / (2.0f * NUM_RINGS));
+        }
+    }
+
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        int w = getWidth();
-        int h = getHeight();
         Paint mRingPaint;
 
         // Draw the pointer and set the ring color.
@@ -69,19 +90,14 @@ public class TeleopView extends View {
                 canvas.drawCircle(mFinger.x, mFinger.y, 30.0f, mPointerPaint);
                 mRingPaint = mRingEnabledPaint;
             } else {
-                canvas.drawCircle(w / 2.0f, h / 2.0f, 30.0f, mRingDisabledPaint);
+                canvas.drawCircle(mWidth / 2.0f, mHeight / 2.0f, 30.0f, mRingDisabledPaint);
                 mRingPaint = mRingDisabledPaint;
             }
         }
 
         // Draw concentric circles to represent the surface.
-        int numRings = 3;
-        for (int i = 0; i < numRings; ++i) {
-            RectF r = new RectF(0, 0, w, h);
-            r.inset(i * w / (2.0f * numRings),
-                    i * h / (2.0f * numRings));
-            canvas.drawOval(r, mRingPaint);
-        }
+        for (int i = 0; i < NUM_RINGS; ++i)
+            canvas.drawOval(mRings[i], mRingPaint);
     }
 
     @Override
@@ -103,12 +119,12 @@ public class TeleopView extends View {
                     } else {
                         mFinger.set(eventX, eventY);
                     }
-                    sendTeleopCommand(mFinger);
+                    new UpdateVelocity().execute(mFinger);
                     break;
                 case MotionEvent.ACTION_UP:
                 case MotionEvent.ACTION_CANCEL:
                     mFinger = null;
-                    sendTeleopCommand(null);
+                    new UpdateVelocity().execute();
                     break;
                 default:
                     return false;
@@ -120,28 +136,33 @@ public class TeleopView extends View {
         return true;
     }
 
-    protected void sendTeleopCommand(PointF pos) {
-        synchronized (mFingerLock) {
-            Twist twist = new Twist();
+    protected class UpdateVelocity extends AsyncTask<PointF, Void, Void> {
 
-            // If there is no vehicle server, don't do anything.
-            if (mServer == null)
-                return;
+        @Override
+        protected Void doInBackground(PointF... finger) {
+            synchronized (mFingerLock) {
+                Twist twist = new Twist();
 
-            // If position is null, just use the zero-velocity command.
-            if (pos == null) {
+                // If there is no vehicle server, don't do anything.
+                if (mServer == null)
+                    return null;
+
+                // If no positions, just use the zero-velocity command.
+                if (finger.length == 0) {
+                    mServer.setVelocity(twist, null);
+                    return null;
+                }
+
+                // Scale the contact point to a velocity between [-1.0, 1.0].
+                double rudder = 2.0f * (finger[0].x / mWidth) - 1.0f;
+                double thrust = -2.0f * (finger[0].y / mHeight) + 1.0f;
+
+                // Send the command to the vehicle.
+                twist.dx(thrust);
+                twist.drz(rudder);
                 mServer.setVelocity(twist, null);
-                return;
+                return null;
             }
-
-            // Scale the contact point to a velocity between [-1.0, 1.0].
-            double rudder = 2.0f * (pos.x / (float)getWidth()) - 1.0f;
-            double thrust = -2.0f * (pos.y / (float)getHeight()) + 1.0f;
-
-            // Send the command to the vehicle.
-            twist.dx(thrust);
-            twist.drz(rudder);
-            mServer.setVelocity(twist, null);
         }
     }
 }
