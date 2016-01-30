3ba260b113c005f174f26a7028572bc0a45d1d0f
Pras Velagapudi <psigen@gmail.com>
2016-01-30 17:18:14 -0500

    Changed log level of disconnection event.

--- src/com/platypus/android/server/Controller.java
+++ src/com/platypus/android/server/Controller.java
@@ -218,7 +218,7 @@ public class Controller {
             mUsbDescriptor = null;
 
             // Clear old accessory references.
-            Log.e(TAG, "Closed " + mUsbAccessory);
+            Log.i(TAG, "Closed " + mUsbAccessory);
             mUsbAccessory = null;
         }
     }
