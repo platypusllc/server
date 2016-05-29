f2b500877075250dfcd1d909f191a50d4b244e3d
Pras Velagapudi <psigen@gmail.com>
2016-05-29 12:55:34 -0400

    Refactored automatic log uploads to use JobScheduler.

--- app/src/main/java/com/platypus/android/server/PlatypusApplication.java
+++ app/src/main/java/com/platypus/android/server/PlatypusApplication.java
@@ -1,13 +1,14 @@
 package com.platypus.android.server;
 
 import android.app.Application;
+
 import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
  * Application configuration for the Platypus Server.
  */
-public class PlatypusApplication extends Application{
+public class PlatypusApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
--- app/src/main/java/com/platypus/android/server/SettingsFragment.java
+++ app/src/main/java/com/platypus/android/server/SettingsFragment.java
@@ -1,19 +1,20 @@
 package com.platypus.android.server;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.util.Log;
+import android.preference.PreferenceManager;
 
 /**
  * Fragment that implements a dynamically-generated settings panel for the server.
- *
+ * <p/>
  * This panel is generated from an XML layout resource, and changes settings in the application
  * SharedPreferences.  These preferences can be dynamically queried by the server, and are
  * automatically saved and preserved across software updates.
  */
 public class SettingsFragment extends PreferenceFragment {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +23,23 @@ public class SettingsFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.preferences);
 
         // Add a click handler to synchronize logs.
-        Preference button = (Preference)findPreference("pref_cloud_sync");
+        Preference button = (Preference) findPreference("pref_cloud_sync");
         button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                Intent logUploadIntent = new Intent(getActivity(), LogUploadService.class);
-                logUploadIntent.setAction(LogUploadService.ACTION_SCAN_AND_UPLOAD_LOGS);
-                getActivity().startService(logUploadIntent);
+                LogUploadService.runSyncNow(getActivity());
                 return true;
             }
         });
+        
+        // Create a shared preference handler to schedule and un-schedule log file auto-uploads.
+        PreferenceManager.getDefaultSharedPreferences(getActivity())
+                .registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+                    @Override
+                    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                        if ("pref_cloud_autosync_enable".equals(key))
+                            LogUploadService.updateAutoSync(getActivity());
+                    }
+                });
     }
 }
